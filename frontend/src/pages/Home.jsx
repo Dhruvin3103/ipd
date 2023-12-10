0ddf532d459dd0286fcf174983fc941828d8c54b
param <param772003@gmail.com>
2023-12-10 15:51:20 +0530

    one more push

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -44,11 +44,11 @@ const Home_page = () => {
         </div>
       </div>
       <div id="map">
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '500px', width: '100%' }}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+          />
           <Marker position={[51.505, -0.09]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
